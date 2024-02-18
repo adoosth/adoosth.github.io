6af626fc8ebb9b09a32176a47cf07b3da4722e14
lilyakh123 <mirzaeem2005@gmail.com>
2024-02-19 03:27:25 +0330

    ‌30-bahman‌

--- ips.py
+++ ips.py
@@ -4,8 +4,8 @@ ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
     'ora-ip': '150.136.246.47',
     'ora-date': '29-bahman',
-    'orb-ip': '150.136.164.56',
-    'orb-date': '29-bahman',
+    'orb-ip': '129.213.215.213',
+    'orb-date': '30-bahman',
     'orz-ip': '129.213.90.59',
     'orz-date': '30-bahman',
 }
