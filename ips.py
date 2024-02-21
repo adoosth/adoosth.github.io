0c4411b8ff6097fd03204824b3ed6b4f6c4e7213
Ali Doosthosseini <doosth.ali@gmail.com>
2024-02-21 01:57:01 +0100

    2 esfand

--- ips.py
+++ ips.py
@@ -4,8 +4,8 @@ ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
     'ora-ip': '132.145.208.198',
     'ora-date': '1-esfand',
-    'orb-ip': '129.213.215.213',
-    'orb-date': '30-bahman',
-    'orz-ip': '129.213.90.59',
-    'orz-date': '30-bahman',
+    'orb-ip': '150.136.31.116',
+    'orb-date': '2-esfand',
+    'orz-ip': '132.145.198.100',
+    'orz-date': '2-esfand',
 }
