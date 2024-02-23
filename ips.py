72b9326fd443644c4da353bd472839ad4354cb8d
lilyakh123 <mirzaeem2005@gmail.com>
2024-02-23 04:51:33 +0330

    4-esfand

--- ips.py
+++ ips.py
@@ -2,7 +2,7 @@ import os
 
 ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
-    'ora-ip': '132.145.215.210 ',
+    'ora-ip': '132.145.215.210',
     'ora-date': '4-esfand',
     'orb-ip': '150.136.160.133',
     'orb-date': '3-esfand',
