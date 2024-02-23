1837c742736f818d37201d4b71fadb40539b640d
lilyakh123 <mirzaeem2005@gmail.com>
2024-02-23 04:44:49 +0330

    4-esfand-ali's birtyhday

--- ips.py
+++ ips.py
@@ -2,8 +2,8 @@ import os
 
 ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
-    'ora-ip': '150.136.122.54',
-    'ora-date': '3-esfand',
+    'ora-ip': '132.145.215.210 ',
+    'ora-date': '4-esfand',
     'orb-ip': '150.136.160.133',
     'orb-date': '3-esfand',
     'orz-ip': '129.213.93.251',
