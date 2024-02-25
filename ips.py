ccb612f59f99723e369dac03856523a4697a5b97
Ali Doosthosseini <doosth.ali@gmail.com>
2024-02-25 14:01:04 +0100

    6 esfand hamrah

--- ips.py
+++ ips.py
@@ -4,7 +4,7 @@ ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
     'ora-ip': '132.145.215.210',
     'ora-date': '4-esfand',
-    'orb-ip': '129.213.121.191',
+    'orb-ip': '129.153.47.119',
     'orb-date': '6-esfand',
     'orz-ip': '129.213.147.83',
     'orz-date': '6-esfand',
