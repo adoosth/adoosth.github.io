b365ab9a87c0a3655fd20f89dab81c96140975dd
lilyakh123 <mirzaeem2005@gmail.com>
2024-03-05 15:57:17 +0330

    14-esfand

--- ips.py
+++ ips.py
@@ -2,10 +2,10 @@ import os
 
 ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
-    'ora-ip': '129.80.153.51',
-    'ora-date': '13-esfand',
-    'orb-ip': '129.153.21.114',
-    'orb-date': '13-esfand',
+    'ora-ip': '132.226.62.161',
+    'ora-date': '14-esfand',
+    'orb-ip': '129.80.27.161',
+    'orb-date': '14-esfand',
     'orz-ip': '129.213.22.27', 
     'orz-date': '10-esfand',
 }
