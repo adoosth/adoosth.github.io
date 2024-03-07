bf668c8660479be408e801585b5601dea60de261
lilyakh123 <mirzaeem2005@gmail.com>
2024-03-08 02:50:50 +0330

    17-esfand

--- ips.py
+++ ips.py
@@ -4,8 +4,8 @@ ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
     'ora-ip': '150.230.180.1',
     'ora-date': '15-esfand',
-    'orb-ip': '150.136.28.205',
-    'orb-date': '16-esfand',
+    'orb-ip': '129.153.44.83',
+    'orb-date': '17-esfand',
     'orz-ip': '129.213.22.27', 
     'orz-date': '10-esfand',
 }
