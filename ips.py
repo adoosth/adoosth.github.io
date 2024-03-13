504ef0343685c9b9264f26082cdf6a6e312cbf87
lilyakh123 <mirzaeem2005@gmail.com>
2024-03-13 03:44:21 +0330

    no space

--- ips.py
+++ ips.py
@@ -4,7 +4,7 @@ ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
     'ora-ip': '150.230.180.1',
     'ora-date': '15-esfand',
-    'orb-ip': '150.136.243.190 ',
+    'orb-ip': '150.136.243.190',
     'orb-date': '23-esfand',
     'orz-ip': '129.213.22.27', 
     'orz-date': '10-esfand',
