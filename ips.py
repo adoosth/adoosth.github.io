41b3e75f265a76eb7179bb7bea366a421f8c1310
lilyakh123 <mirzaeem2005@gmail.com>
2024-03-21 01:15:29 +0330

    2-Farvardin

--- ips.py
+++ ips.py
@@ -4,8 +4,8 @@ ip_dict = {
     'ncb-ipv6': '[2a03:4000:4c:22:d899:cdff:fe6d:66e0]',
     'ora-ip': '150.136.171.168',
     'ora-date': '26-esfand',
-    'orb-ip': '129.80.210.197',
-    'orb-date': '1-farvardin',
+    'orb-ip': '129.213.123.170',
+    'orb-date': '2-farvardin',
     'orz-ip': '129.213.22.27', 
     'orz-date': '10-esfand',
 }
